8f4778db1a4134a3712c6dc041fb577e005a4b89
dev-team-at-226946988891 <zulqarnain@ecotechitservices.com>
2021-05-23 21:46:16 +0530

    Demo Created

---
+++ POCDemo/POCDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  POCDemo
+//
+//  Created by iOS on 23/05/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
